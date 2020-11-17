b50e294256c07d6fb52ce999afd8373d59dc5bc5
Mikkel Paulson <git@email.mikkel.ca>
2020-11-17 14:30:23 -0500

    apply eslint to proxy.js

--- proxy.js
+++ proxy.js
@@ -1,69 +1,73 @@
-const http = require("http");
-const https = require("https");
-const yargs = require("yargs/yargs");
+const http = require('http');
+const https = require('https');
+const yargs = require('yargs/yargs');
 
 const argv = yargs(process.argv.slice(2))
-    .command("* <feed>", "Start the proxy server", (yargs) => {
-        yargs.positional("feed", {
-            describe: "The feed URL to proxy",
-            type: "string"
-        })
+    .command('* <feed>', 'Start the proxy server', (yargs) => {
+      yargs.positional('feed', {
+        describe: 'The feed URL to proxy',
+        type: 'string',
+      });
     })
     .options({
-        "ip": {
-            default: "127.0.0.1",
-            describe: "IP address to listen on",
-            type: "string"
-        },
-        "port": {
-            default: 43641,
-            describe: "Port to listen on",
-            type: "number"
-        },
-        "minutes": {
-            default: 10,
-            describe: "Minutes before the event to post a reminder",
-            type: "number"
-        }
+      'ip': {
+        default: '127.0.0.1',
+        describe: 'IP address to listen on',
+        type: 'string',
+      },
+      'port': {
+        default: 43641,
+        describe: 'Port to listen on',
+        type: 'number',
+      },
+      'minutes': {
+        default: 10,
+        describe: 'Minutes before the event to post a reminder',
+        type: 'number',
+      },
     })
     .argv;
 
-const server = http.createServer((req, res) => {
-    console.log("Received request");
+http.createServer((req, res) => {
+  console.log('Received request');
 
-    https.get(argv.feed, (queryRes) => {
-        var data = "";
+  https.get(argv.feed, (queryRes) => {
+    let data = '';
 
-        queryRes.on("data", (chunk) => {
-            data += chunk;
-        });
-
-        queryRes.on("end", () => {
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/calendar; charset=utf-8");
-            var processedData = addReminders(data);
-            res.end(processedData);
-            console.log("Success (length " + processedData.length + ")");
-        });
-    }).on("error", (error) => {
-        res.statusCode = 500;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Error: " + error.message);
-        console.log("Error (" + error.message + ")");
+    queryRes.on('data', (chunk) => {
+      data += chunk;
     });
+
+    queryRes.on('end', () => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
+      const processedData = addReminders(data);
+      res.end(processedData);
+      console.log('Success (length ' + processedData.length + ')');
+    });
+  }).on('error', (error) => {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Error: ' + error.message);
+    console.log('Error (' + error.message + ')');
+  });
 }).listen(argv.port, argv.ip, () => {
-    console.log("Listening on " + argv.ip + ":" + argv.port + "...");
+  console.log('Listening on ' + argv.ip + ':' + argv.port + '...');
 });
 
 
+/**
+ * @param {string} input
+ * @return {string}
+ */
 function addReminders(input) {
-    return input.replace(
-        /^SUMMARY:(.*)$/gm,
-        "SUMMARY:$1\r\n" +
-        "BEGIN:VALARM\r\n" +
-        "TRIGGER:-PT" + argv.minutes + "M\r\n" +
-        "ACTION:DISPLAY\r\n" +
-        "DESCRIPTION:$1\r\n" +
-        "END:VALARM"
-    );
+  return input.replace(
+      /^SUMMARY:(.*)$/gm,
+      'SUMMARY:$1\r\n' +
+      'BEGIN:VALARM\r\n' +
+      'TRIGGER:-PT' + argv.minutes + 'M\r\n' +
+      'ACTION:DISPLAY\r\n' +
+      'DESCRIPTION:$1\r\n' +
+      'END:VALARM',
+  );
 }
