b67dee8a58590d4966323a1b4e91b8d171738291
Mikkel Paulson <git@email.mikkel.ca>
2020-11-18 11:50:11 -0500

    rough in POST and PROPFIND methods

--- app.js
+++ app.js
@@ -30,24 +30,50 @@ const argv = yargs(process.argv.slice(2))
     })
     .argv;
 
-app.get('/', (req, res) => {
-  console.log('Received request');
-
-  axios.get(argv.feed)
-      .then((sourceResult) => {
+app.route('/')
+    .all((req, res, next) => {
+      console.log(req.method + ' /');
+      if (req.method == 'PROPFIND') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
-        const processedData = addReminders(sourceResult.data);
-        res.end(processedData);
-        console.log('Success (length ' + processedData.length + ')');
-      })
-      .catch((error) => {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Error: ' + error.message);
-        console.log('Error (' + error.message + ')');
+        res.end('Um, hi!\r\n');
+        // PROPFIND HERE
+      } else {
+        next();
+      }
+    })
+    .get((req, res) => {
+      axios.get(argv.feed)
+          .then((sourceResult) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
+            const processedData = addReminders(sourceResult.data);
+            res.end(processedData);
+            console.log(res.getHeaders());
+            console.log('Success (length ' + processedData.length + ')');
+          })
+          .catch((error) => {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Error: ' + error.message);
+            console.log('Error (' + error.message + ')');
+          });
+    })
+    .put((req, res) => {
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk;
+      }).on('end', () => {
+        console.log('Body: ' + body);
+        res.statusCode = 200;
+        // res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
+        // const processedData = addReminders(sourceResult.data);
+        // res.end(processedData);
+        // console.log(res.getHeaders());
+        // console.log('Success (length ' + processedData.length + ')');
       });
-});
+    });
 
 app.listen(argv.port, argv.ip, () => {
   console.log('Listening on ' + argv.ip + ':' + argv.port + '...');
@@ -63,8 +89,8 @@ function addReminders(input) {
       /^SUMMARY:(.*)$/gm,
       'SUMMARY:$1\r\n' +
       'BEGIN:VALARM\r\n' +
-      'TRIGGER:-PT' + argv.minutes + 'M\r\n' +
       'ACTION:DISPLAY\r\n' +
+      'TRIGGER;VALUE=DURATION:-PT' + argv.minutes + 'M\r\n' +
       'DESCRIPTION:$1\r\n' +
       'END:VALARM',
   );
